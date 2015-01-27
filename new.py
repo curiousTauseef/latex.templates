37b52c36c03f6149d1f5747d31d2baa9b3cfd9b9
Jinghao Shi <jinghaos@buffalo.edu>
2015-01-27 17:19:28 -0500

    exclude .git dir in templates

--- new.py
+++ new.py
@@ -5,7 +5,7 @@ import argparse
 import subprocess
 
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
-TEMPLATES = [t for t in os.listdir(ROOT_DIR) if os.path.isdir(os.path.join(ROOT_DIR, t)) and t != 'common']
+TEMPLATES = [t for t in os.listdir(ROOT_DIR) if os.path.isdir(os.path.join(ROOT_DIR, t)) and t not in ['common', '.git']]
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--dest', default=os.getcwd(), help="Destination directory.")
