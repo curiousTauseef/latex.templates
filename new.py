a8511fc9d5d9d844be7e6fddf5b4396b13cab52d
Jinghao Shi <jinghaos@buffalo.edu>
2017-02-01 12:41:59 -0500

    new script

--- new.py
+++ new.py
@@ -5,20 +5,34 @@ import argparse
 import subprocess
 
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
-TEMPLATES = [t for t in os.listdir(ROOT_DIR) if os.path.isdir(os.path.join(ROOT_DIR, t)) and t not in ['common', '.git']]
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--dest', default=os.getcwd(), help="Destination directory.")
-parser.add_argument('template', choices=TEMPLATES, help="Template to create.")
+TEMPLATES = [t for t in os.listdir(ROOT_DIR)
+             if os.path.isdir(os.path.join(ROOT_DIR, t)) and
+             t not in ['common', '.git']]
 
 
-args = parser.parse_args()
+def argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('template', choices=TEMPLATES,
+                        help="Template to create.")
+    parser.add_argument('-d', '--dest', default=os.getcwd(),
+                        help="Destination directory.")
+    return parser
 
-try:
-  os.makedirs(os.path.join(args.dest, 'figs'))
-except:
-  pass
 
-for dir in ['common', args.template]:
-  for file in os.listdir(os.path.join(ROOT_DIR, dir)):
-    subprocess.check_call('cp -riv %s %s' % (os.path.join(ROOT_DIR, dir, file), os.path.join(args.dest)), shell=True)
+def main():
+    args = argparser.parse_args()
+
+    try:
+        os.makedirs(os.path.join(args.dest, 'figs'))
+    except:
+        pass
+
+    for dir in ['common', args.template]:
+        for f in os.listdir(os.path.join(ROOT_DIR, dir)):
+            subprocess.check_call('cp -riv %s %s' %
+                                  (os.path.join(ROOT_DIR, dir, f),
+                                   os.path.join(args.dest)), shell=True)
+
+
+if __name__ == '__main__':
+    main()
